2f75a8da78613d7c8ce005409b08b1a258c295b4
AbhishekVerma2021 <2001avverma@gmail.com>
2022-02-17 23:56:15 +0530

    Map Implimentation

---
+++ src/MapImplementation.java
@@ -0,0 +1,32 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.Set;
+
+public class MapImplementation
+{
+
+    static Map<String,String> map= new HashMap<>();
+    public void saveCountryCapital(String count,String cap)
+    {
+        map.put(count,cap);
+    }
+    public static void main(String[] args)
+    {
+        Scanner sc= new Scanner(System.in);
+
+        MapImplementation obj=new MapImplementation();
+        String cap,count;
+        for(int i=0;i<3;i++)
+        {
+            cap=sc.nextLine();
+            count=sc.nextLine();
+            obj.saveCountryCapital(count,cap);
+        }
+        Set<String> st =map.keySet();
+        for(String k : st)
+        {
+            System.out.println(k+"  =  "+map.get(k));
+        }
+    }
+}
--- src/PriorityQueueLinkedList.java
+++ src/PriorityQueueLinkedList.java
@@ -1,57 +1,57 @@
-public class PriorityQueueLinkedList {
-    Node front;
-    Node rear;
-    Node next;
-    static class Node
-    {
-        Node front;
-        Node rear;
-        Node next;
-    }
-    public PriorityQueueLinkedList() {
-        front = null;
-        rear = null;
-
-    }
-    public void enqueue(Node newnode) {
-        if (front == null || newnode.priority < front.priority) {
-            newnode.next = front;
-            front = newnode;
-        } else {
-            Node temp = front;
-
-            while (temp.next != null && temp.next.priority <= newnode.priority) {
-                temp = temp.next;
-            }
-            newnode.next = temp.next;
-            temp.next = newnode;
-
-        }
-    }
-    public void display()
-    {
-        Node temp = front;
-        while(temp != null)
-        {
-            System.out.println(temp.data + " " + temp.priority);
-            temp = temp.next;
-        }
-    }
-
-
-
-}
-public class PriorityQueueLinkedListMain {
-    public static void main(String[] args) {
-        // Creation of the node class object
-        Node n1= new Node(15,4);
-        Node n2 = new Node(10,3);
-        Node n3 = new Node(20,3);
-        PriorityQueueLinkedList obj = new  PriorityQueueLinkedList();
-        obj.enqueue(n1);
-        obj.enqueue(n2);
-        obj.enqueue(n3);
-        obj.display();
-
-    }
-}
+//public class PriorityQueueLinkedList {
+//    Node front;
+//    Node rear;
+//    Node next;
+//    static class Node
+//    {
+//        Node front;
+//        Node rear;
+//        Node next;
+//    }
+//    public PriorityQueueLinkedList() {
+//        front = null;
+//        rear = null;
+//
+//    }
+//    public void enqueue(Node newnode) {
+//        if (front == null || newnode.priority < front.priority) {
+//            newnode.next = front;
+//            front = newnode;
+//        } else {
+//            Node temp = front;
+//
+//            while (temp.next != null && temp.next.priority <= newnode.priority) {
+//                temp = temp.next;
+//            }
+//            newnode.next = temp.next;
+//            temp.next = newnode;
+//
+//        }
+//    }
+//    public void display()
+//    {
+//        Node temp = front;
+//        while(temp != null)
+//        {
+//            System.out.println(temp.data + " " + temp.priority);
+//            temp = temp.next;
+//        }
+//    }
+//
+//
+//
+//}
+//public class PriorityQueueLinkedListMain {
+//    public static void main(String[] args) {
+//        // Creation of the node class object
+//        Node n1= new Node(15,4);
+//        Node n2 = new Node(10,3);
+//        Node n3 = new Node(20,3);
+//        PriorityQueueLinkedList obj = new  PriorityQueueLinkedList();
+//        obj.enqueue(n1);
+//        obj.enqueue(n2);
+//        obj.enqueue(n3);
+//        obj.display();
+//
+//    }
+//}
